d706501888156bf81cef4c48158d9c678341e866
aminb <amin.burhan@gmail.com>
2020-10-12 01:06:56 +0200

    Adds List of all Owners on Owner index Page and updates owner controller. Closes #15

--- pet-clinic-web/src/main/java/se/aminburhan/aminpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/se/aminburhan/aminpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package se.aminburhan.aminpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.aminburhan.aminpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
