7795113b6e54196f6b1231eace9debfbdbcc79da
Amin <amin.burhan@gmail.com>
2021-08-10 02:21:06 +0200

    Add JavaFaker library to generate random bootstrap data

--- pet-clinic-web/src/main/java/maburhan/aminpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/maburhan/aminpetclinic/controllers/OwnerController.java
@@ -1,10 +1,9 @@
 package maburhan.aminpetclinic.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import maburhan.aminpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import maburhan.aminpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
