75528eabc70c7c9d9d97b750dc417d86cd06f028
Amin <amin.burhan@gmail.com>
2021-09-20 03:13:52 +0200

    Load PetTypes at startup. Closes #32.

--- pet-clinic-web/src/main/java/maburhan/aminpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/maburhan/aminpetclinic/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package maburhan.aminpetclinic.bootstrap;
 
 import com.github.javafaker.Faker;
 import maburhan.aminpetclinic.model.Owner;
+import maburhan.aminpetclinic.model.PetType;
 import maburhan.aminpetclinic.model.Vet;
 import maburhan.aminpetclinic.services.OwnerService;
+import maburhan.aminpetclinic.services.PetTypeService;
 import maburhan.aminpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCat = petTypeService.save(cat);
+
+        System.out.println("Loaded Pet Types....");
 
         Faker faker = new Faker();
 
