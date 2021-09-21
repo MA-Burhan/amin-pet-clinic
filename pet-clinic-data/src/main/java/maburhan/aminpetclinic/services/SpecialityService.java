901ffb1d0bfb0c4361021c70b0f37029797a3fb8
Amin <amin.burhan@gmail.com>
2021-09-21 15:30:07 +0200

    Implement map-based Speciality service implementation. Closes #31.

---
+++ pet-clinic-data/src/main/java/maburhan/aminpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package maburhan.aminpetclinic.services;
+
+import maburhan.aminpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/maburhan/aminpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,9 @@
+package maburhan.aminpetclinic.services.map;
+
+import maburhan.aminpetclinic.model.Speciality;
+import maburhan.aminpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality> implements SpecialityService {
+}
