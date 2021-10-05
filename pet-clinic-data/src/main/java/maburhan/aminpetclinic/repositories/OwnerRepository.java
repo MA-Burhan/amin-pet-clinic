333ac23eb63b714455ea569ccc87d7622fe89aae
Amin <amin.burhan@gmail.com>
2021-10-05 16:06:26 +0200

    Implement Spring Data JPA based Service for Owners. Closes #40.

--- pet-clinic-data/src/main/java/maburhan/aminpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/maburhan/aminpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,9 @@ package maburhan.aminpetclinic.repositories;
 import maburhan.aminpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
 }
